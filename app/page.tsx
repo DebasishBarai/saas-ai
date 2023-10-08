f7768319ccae55e911aa1eab2acd9fa18fceb6f8
Debasish barai <debasishbaraiju@gmail.com>
2023-10-08 19:57:32 +0530

    @/components/ui/sheet.tsx modified

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { Button } from '@/components/ui/button';
+import { auth } from '@clerk/nextjs';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
 const Home = () => {
+  const user = auth();
+
+  if (user) {
+    redirect('/dashboard');
+  }
   return (
     <main>
       <h1>Saas Ai</h1>
-      <Link href="/sign-in">
+      <Link href='/sign-in'>
         <Button>SignIn</Button>
       </Link>
-      <Link href="/sign-up">
+      <Link href='/sign-up'>
         <Button>Register</Button>
       </Link>
     </main>
